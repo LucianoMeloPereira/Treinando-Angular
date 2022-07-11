8e062ac5ddfcba85eb328b26927a68dc7dbdb2d3
LucianoMelo <luciano.mrbuch@gmail.com>
2022-07-11 02:20:28 -0300

    Todo List em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public umaString: string = 'Minhas tarefas';
+  public form!: FormGroup;
+  public mode: string = 'list';
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.required,
+        ]),
+      ],
+    });
+    this.load();
+  }
+
+  changeMode(mode: string){ 
+    this.mode = mode;
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  alteraTexto() {
+    this.umaString = 'Alterei meu texto';
+  }
+
+  clear(){
+    this.form.reset();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index != -1) {
+      this.todos.splice(index, 1);
+      this.save();
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUnDone(todo: Todo) {
+    todo.done = false;
+    this.save();
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.mode = 'list';
+  }
+
+  load(){
+
+    const data = localStorage.getItem('todos');
+
+    if (data) {
+      this.todos = JSON.parse(data);
+    }
+    else{
+      this.todos = [];
+    }
+
+  }
 }
